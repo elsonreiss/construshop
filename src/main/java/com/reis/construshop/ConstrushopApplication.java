c09c6443fc3f3ab8da1f990c001a856e4dfe86e8
Elson Reis <elsonreis084@gmail.com>
2026-04-16 21:05:50 -0300

    chore: initial project setup

---
+++ src/main/java/com/reis/construshop/ConstrushopApplication.java
@@ -0,0 +1,12 @@
+package com.reis.construshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstrushopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstrushopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/reis/construshop/ConstrushopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reis.construshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstrushopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
